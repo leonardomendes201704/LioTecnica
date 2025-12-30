870cd9efefaa8fcd1ac785481325eea30d4f38dd
Leonardo Mendes <ter.leonardo.mendes@sada.com.br>
2025-12-30 15:33:59 -0300

    feat: agenda

---
+++ LioTecnica.Web/Controllers/AgendasController.cs
@@ -0,0 +1,20 @@
+﻿using LioTecnica.Web.Helpers;
+using LioTecnica.Web.Services;
+using LioTecnica.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LioTecnica.Web.Controllers
+{
+    public class AgendasController : Controller
+    {
+        public IActionResult Index()
+        {
+            var seed = MockDataService.BuildSeedBundle();
+            var model = new PageSeedViewModel
+            {
+                SeedJson = SeedJsonHelper.ToJson(seed)
+            };
+            return View(model);
+        }
+    }
+}
---
+++ LioTecnica.Web/Views/Agendas/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LioTecnica.Web.Views.Agenda
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
